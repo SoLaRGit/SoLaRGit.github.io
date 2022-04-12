59a4173a436d86a3d60a9079066421c184ae8c9d
Nikola Bozovic <nigerija@gmail.com>
2022-04-12 20:04:01 +0200

    open.hpc.help

---
+++ docs/open.hpc/scripts/bugfix.js
@@ -0,0 +1,45 @@
+﻿// JScript File
+
+// HH.exe does not open links with anchor hash #anchor
+
+function goHash()
+{
+  var srcElement = event.srcElement;
+  if (srcElement)
+  {
+    var href = srcElement.getAttribute("href");
+    var target = srcElement.getAttribute("target");
+    window.open(href, target);
+    return false;
+  }
+}
+
+function exec()
+{
+  try
+  {
+    var texteval = document.getElementById("texteval");
+    eval(texteval.innerText);
+  }
+  catch (ex)
+  {
+    // TODO: better handing
+    alert(ex);
+  }
+}
+
+function showEval()
+{
+  try
+  {
+    var div = document.getElementById("evaldiv");
+    div.innerHTML = '<textarea id="texteval" cols="140" rows="20"></textarea><br/><button onclick="exec()">exec</button>';
+  }
+  catch (ex)
+  {
+    // TODO: better handing
+    alert(ex);
+  }
+
+}
+
